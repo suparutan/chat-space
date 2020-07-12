fa89540d6a47455d88c69e88f04ed09694cfc154
suparutan <66267782+suparutan@users.noreply.github.com>
2020-07-12 15:42:06 +0900

    Revert "auto"
    This reverts commit 2613d67adddb2a62a71a727983b3e450e8e27174.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,70 +1,68 @@
-$(function() { 
+$(function(){ 
   function buildHTML(message){
     if ( message.image ) {
-        var html =
-        `<div class="chat-main__message-list" data-message-id=${message.id}>
-            <div class="chat-main__message-list__user">
-              <div class="chat-main__message-list__user__left">
-                ${message.user_name}
-              </div>
-              <div class="chat-main__message-list__user__right">
-                ${message.created_at}
-              </div>
+      var html =
+        `<div class="chat-main">
+          <div class="chat-main__message-list__user">
+            <div class="chat-main__message-list__user__left">
+              ${message.user_name}
             </div>
-            <div class="lower-message">
-              <p class="chat-main__message-list__past">
-                ${message.content}
-              </p>
+            <div class="chat-main__message-list__user__right">
+              ${message.created_at}
             </div>
-            <img src=${message.image} >
-          </div>`
-        return html;
-      } else {
-        var html =
-        `<div class="chat-main__message-list" data-message-id=${message.id}>
-            <div class="chat-main__message-list__user">
-              <div class="chat-main__message-list__user__left">
-                ${message.user_name}
-              </div>
-              <div class="chat-main__message-list__user__right">
-                ${message.created_at}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="chat-main__message-list__past">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
+          </div>
+          <div class="lower-message">
+            <p class="chat-main__message-list__past">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =`<div class="chat-main">
+      <div class="chat-main__message-list__user">
+        <div class="chat-main__message-list__user__left">
+          ${message.user_name}
+        </div>
+        <div class="chat-main__message-list__user__right">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="lower-message">
+        <p class="chat-main__message-list__past">
+          ${message.content}
+        </p>
+      </div>
+      </div>`
+      return html;
     };
-  };
-  $('#new_message').on('submit', function(e){
-    e.preventDefault()
-    var reloadMessages = function() {
-      var last_message_id = $('.message:last').data("message-id");
-      $.ajax({
-        url: "api/messages",
-        type: 'get',
-        dataType: 'json',
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        if (messages.length !== 0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        $('.messages').append(insertHTML);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      }
-      })
-      .fail(function() {
-        alert('error');
-      });
-    };
-  });
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
   }
+$('#new_message').on('submit', function(e){
+  console.log('hoge');
+  e.preventDefault()
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.chat-main__message-list').append(html);
+    $('form')[0].reset();
+    $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+    
+  })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+  })
+  .always(function(){
+  $('.submit-btn').prop('disabled', false);
+  });
+});
 });
